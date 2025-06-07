a84a936d41c2eba5885981a83fe666df6b765377
Kalum Gunawardhana <kguna415@gmail.com>
2025-06-07 20:05:05 +0530

    commit

---
+++ src/main/java/edu/ecommerce/controller/ProductController.java
@@ -0,0 +1,40 @@
+package edu.ecommerce.controller;
+
+import edu.ecommerce.entity.Product;
+import edu.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return service.getProductById(id);
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return service.createProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(@PathVariable Long id, @RequestBody Product product) {
+        return service.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.deleteProduct(id);
+    }
+}

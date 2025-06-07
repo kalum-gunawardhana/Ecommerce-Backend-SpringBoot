a7b25797c684fb55fc51b5aa1089df0595c61d8c
Kalum Gunawardhana <kguna415@gmail.com>
2025-06-07 19:37:12 +0530

    commit

--- src/main/java/edu/ecommerce/enums/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package edu.ecommerce.enums;
-
-public enum OrderStatus {
-    PENDING, CONFIRMED, SHIPPED, DELIVERED, CANCELLED, RETURNED
-}
--- src/main/java/edu/ecommerce/enums/UserRole.java
+++
@@ -1,5 +0,0 @@
-package edu.ecommerce.enums;
-
-public enum UserRole {
-    ADMIN, USER
-}
